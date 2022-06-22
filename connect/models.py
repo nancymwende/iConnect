dd7673be9fdc26d286a4b436b642b392cc48a677
“Edu58” <edumuriithi58@gmail.com>
2022-06-22 08:54:43 +0300

    adds Profile model

--- connect/models.py
+++ connect/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+gender_choices = (
+    ('male', 'male'),
+    ('female', 'female'),
+)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to='photos/', null=True, blank=True)
+    age = models.IntegerField(null=False, blank=False)
+    ethnicity = models.CharField(max_length=30, null=True, blank=True)
+    gender = models.CharField(choices=gender_choices, max_length=30, null=False, blank=False)
+    location = models.CharField(max_length=30, null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    preference = models.TextField(max_length=500, blank=True, null=True)
